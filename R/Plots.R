b19dfc53aea0ea6b652ecbb66fe9f270f1e0f025
vullioud <colino.vullioud@gmail.com>
2018-07-19 15:41:22 +0200

    edit the code and names of plot1 and plot2 functions

--- R/Plots.R
+++ R/Plots.R
@@ -1,8 +1,8 @@
-#'plot1 panel (used internally)
+#'plot relat panel (used internally)
 #'
-#'Builds the panel of plot1.
+#'Builds the panel of plot2.
 #'
-#'@name plot1_inside
+#'@name plot_relat_inside
 #'@param DF, data frame to be plotted: females_relat, migrants_relat or natives_relat
 #'@param legend, include or not the axis legend (logical)
 #'@param labels_x_axis, vector of character to specify the axis labels
@@ -11,70 +11,77 @@
 #'@export
 #'@examples
 #'data(females_relat)
-#'plot1_inside(DF = females_relat,
+#'plot_relat_inside(DF = females_relat,
 #'            legend = FALSE,
 #'            labels_x_axis = c("test", "test"))
-plot1_inside <- function(DF,
+plot_relat_inside <- function(DF,
                       legend,
                       labels_x_axis) {
 time <- relat <- NULL
 
   col1 <- c("#009E73",  "#9ad0f3", "#F0E442", "#D55E00")
-tt2 <- ggplot(DF, aes(time, y = relat,fill = time, col = time), alpha = 0.02) +
-  geom_boxplot(width = 0.4, outlier.shape = NA) +
-  stat_summary(geom = "crossbar",
+tt2 <- ggplot2::ggplot(DF, ggplot2::aes(time, y = relat,fill = time, col = time), alpha = 0.02) +
+  ggplot2::geom_boxplot(width = 0.4, outlier.shape = NA) +
+  ggplot2::stat_summary(geom = "crossbar",
                width=0.50,
                fatten=0,
                color="black",
                fun.data = function(x){ return(c(y=stats::median(x),
                                                 ymin = stats::median(x),
                                                 ymax = stats::median(x)))}) +
-  scale_y_continuous("Cumulative relatedness",
+  ggplot2::scale_y_continuous("Cumulative relatedness",
                      limits = c(0, 6.6)) +
-  scale_x_discrete("",
+  ggplot2::scale_x_discrete("",
                    labels = labels_x_axis) +
-  scale_color_manual(values = col1) +
-  scale_fill_manual(values = col1) +
-  geom_text(data = DF, aes(x = 1.5, y = 6.6, label = paste(DF$type[1])),
+  ggplot2::scale_color_manual(values = col1) +
+  ggplot2::scale_fill_manual(values = col1) +
+  ggplot2::geom_text(data = DF, ggplot2::aes(x = 1.5, y = 6.6, label = paste(DF$type[1])),
             size = 0.35*7,
             col = "black",
             inherit.aes = F,
-            check_overlap = T) +
-  geom_hline(yintercept = 6.3)
+            check_overlap = T,
+            family = "",
+            fontface = "plain") +
+  ggplot2::geom_hline(yintercept = 6.3)
 
 if(legend == FALSE) {
   tt2 <- tt2 +
     ggthemes::theme_base() +
-    guides(color = FALSE, fill = FALSE) +
-    theme(text = element_text(size = 7),
-          axis.ticks.length = unit(1, "mm"),
-          axis.title.x = element_blank(),
-          plot.background = element_blank(),
-          axis.title.y = element_blank(),
-          axis.text.y = element_blank(),
-          axis.ticks.y = element_blank())
+    ggplot2::guides(color = FALSE, fill = FALSE) +
+    ggplot2::theme(text = ggplot2::element_text(size = 7, 
+                                                family = "",
+                                                face = "plain") ,
+          axis.ticks.length = ggplot2::unit(1, "mm"),
+          axis.title.x = ggplot2::element_blank(),
+          plot.background = ggplot2::element_blank(),
+          axis.title.y = ggplot2::element_blank(),
+          axis.text.y = ggplot2::element_blank(),
+          axis.ticks.y = ggplot2::element_blank())
 } else {
   tt2 <- tt2 +
     ggthemes::theme_base() +
-    guides(color = FALSE, fill = FALSE) +
-    theme(text = element_text(size = 7),
-          axis.ticks.length = unit(1, "mm"),
-          axis.title.x = element_blank(),
-          plot.background = element_blank())
+    ggplot2::guides(color = FALSE, fill = FALSE) +
+    ggplot2::theme(text = ggplot2::element_text(size = 7, 
+                                                family = "",
+                                                face = "plain"),
+          axis.ticks.length = ggplot2::unit(1, "mm"),
+          axis.title.x = ggplot2::element_blank(),
+          plot.background = ggplot2::element_blank())
 }
 return(tt2)
 }
 
 
 ################################################################################
-#'plot1
+#'second plot
 #'
-#'Creates the plot1 of the paper.
+#'Creates the second plot of the paper.
 #'
-#'@name plot1
+#'@name plot2
 #'@param DF_female, females_relat
 #'@param DF_migrant, migrants_relat
 #'@param DF_native, natives_relat
+#'@param PDF, a logical to return a PDF version of the plot 
 #'@import ggplot2
 #'@import ggthemes
 #'@importFrom egg "ggarrange"
@@ -83,39 +90,56 @@ return(tt2)
 #'data(females_relat)
 #'data(migrants_relat)
 #'data(natives_relat)
-#'plot1(DF_female = females_relat,
+#'plot2(DF_female = females_relat,
 #'      DF_migrant = migrants_relat,
-#'      DF_native = natives_relat)
+#'      DF_native = natives_relat, 
+#'      PDF = TRUE)
 
-plot1 <- function(DF_female, DF_migrant, DF_native){
+plot2 <- function(DF_female, DF_migrant, DF_native, PDF){
 
-Plot_F <- plot1_inside(DF_female,
+Plot_F <- plot_relat_inside(DF_female,
                     legend = FALSE,
                     labels_x_axis = c("2.5 yrs of age", "4.5 yrs of age")) +
-  theme(plot.margin = margin(t = 3, r = 3, b = 3, l = 0, "pt"),
-        axis.text.x = element_text(angle = 60, hjust = 1))
+  ggplot2::theme(plot.margin = ggplot2::margin(t = 3, r = 3, b = 3, l = 0, "pt"),
+        axis.text.x = ggplot2::element_text(angle = 60,
+                                            hjust = 1, 
+                                            size = 7, 
+                                            family = "",
+                                            face = "plain"))
 
-Plot_M <- plot1_inside(DF_migrant,
+Plot_M <- plot_relat_inside(DF_migrant,
                     legend = TRUE,
                     labels_x_axis = c("1yr before dispersal",
                                       "1yr after dispersal")) +
-  theme(plot.margin = margin(t = 3, r = 0, b = 3, l = 3, "pt"),
-        axis.text.x = element_text(angle = 60, hjust = 1))
+  ggplot2::theme(plot.margin = ggplot2::margin(t = 3, r = 0, b = 3, l = 3, "pt"),
+        axis.text.x = ggplot2::element_text(angle = 60, 
+                                            hjust = 1, 
+                                            size = 7, 
+                                            family = "",
+                                            face = "plain"))
 
-Plot_philo <- plot1_inside(DF_native,
+Plot_philo <- plot_relat_inside(DF_native,
                            legend = FALSE,
                            labels_x_axis = c("1yr before onset of\n reproductive activity",
                                              "1yr after onset of\n reproductive activity")) +
-  theme(plot.margin = margin(t = 3, r = -1, b = 3, l = -1, "pt"),
-        axis.text.x = element_text(angle = 60, hjust = 1))
+  ggplot2::theme(plot.margin = ggplot2::margin(t = 3, r = -1, b = 3, l = -1, "pt"),
+        axis.text.x = ggplot2::element_text(angle = 60, 
+                                            hjust = 1, 
+                                            size = 7, 
+                                            family = "",
+                                            face = "plain"))
 
 x2 <- egg::ggarrange(Plot_M, Plot_philo,Plot_F, ncol = 3)
+if(PDF == TRUE) {
+  ggplot2::ggsave(x2, filename = "plot2.pdf", width = 88, height = 68, units= "mm")
+} else {
 return(x2)
+}
 }
 
 ################################################################################
 ################################################################################
-#' Plot2 mass and social panels (used internally)
+#' Plot prediction for mass and social panels (used internally)
 #'
 #' Plots the predictions for the mass and social models. These
 #' predictions are computed and formated inside the plot2 function
@@ -129,26 +153,31 @@ plot_intra_inter <- function(DF) {
   pred <- pos2 <- type <- Model <- inf <- sup <- NULL
   PPA <- c("#009E73",  "#FB9A99","#9ad0f3", "#D55E00")
 
-  ggplot(DF, aes(y = pred, x = pos2, col = type, shape = Model)) +
-    geom_point() +
-    geom_hline(yintercept = c(0.25,  0.75), linetype = 3, size = 0.2) +
-    geom_hline(yintercept = 0.5, linetype = 3, size = 0.5, col = "black") +
-    geom_errorbar(aes(ymin = inf, ymax = sup), width= 0) +
-    geom_text(data = DF, aes(x = 2.5, y = 1.10), label = paste(DF$plotname[1]), size = 7/(14/5), col = "black", inherit.aes = F, check_overlap = TRUE) +
-    geom_hline(yintercept = 1.05) +
-    scale_color_manual(" ",
+  ggplot2::ggplot(DF, ggplot2::aes(y = pred, x = pos2, col = type, shape = Model)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_hline(yintercept = c(0.25,  0.75), linetype = 3, size = 0.2) +
+    ggplot2::geom_hline(yintercept = 0.5, linetype = 3, size = 0.5, col = "black") +
+    ggplot2::geom_errorbar(ggplot2::aes(ymin = inf, ymax = sup), width= 0) +
+    ggplot2::geom_text(data = DF, ggplot2::aes(x = 2.5, y = 1.10), 
+              label = paste(DF$plotname[1]), 
+              size = 0.35*7, 
+              col = "black", 
+              inherit.aes = F, 
+              check_overlap = TRUE) +
+    ggplot2::geom_hline(yintercept = 1.05) +
+    ggplot2::scale_color_manual(" ",
                        values = PPA) +
-    scale_shape_manual(" ",
+    ggplot2::scale_shape_manual(" ",
                        values = c(15, 19))+
-    scale_x_continuous(limits = c(0.85,4.10),
+    ggplot2::scale_x_continuous(limits = c(0.85,4.10),
                        breaks = c(1, 2, 3, 4),
                        labels =  c("Interclan",  "Intra-Mixed", "Intra-Native", "Intra-Immigrant")) +
-    scale_y_continuous("Winning probability (%)",
+    ggplot2::scale_y_continuous("Winning probability (%)",
                        limits = c(0,1.10),
                        breaks = c(0, 0.25, 0.5, 0.75, 1),
                        labels = c("0", "25", "50", "75", "100")) +
     ggthemes::theme_base()  +
-    guides(col = FALSE, shape = FALSE)
+    ggplot2::guides(col = FALSE, shape = FALSE)
 }
 
 ################################################################################
@@ -167,40 +196,46 @@ plot_sex <- function(DF) {
   inf <- sup <- Model <- pred <- pos <- type <- NULL
   PPA_intra <- c("#009E73",  "#9ad0f3", "#D55E00")
 
-  ggplot(DF, aes(y = pred, x = pos, col = type, shape = Model)) +
-    geom_point() +
-    geom_hline(yintercept = c(0.25,  0.75), linetype = 3, size = 0.2) +
-    geom_hline(yintercept = 0.5, linetype = 3, size = 0.5, col = "black") +
-    geom_errorbar(aes(ymin = inf, ymax = sup), width= 0) +
-    geom_text(data = DF, aes(x = 2, y = 1.10), label = paste(DF$plotname[1]), size = 7/(14/5), col = "black", inherit.aes = FALSE, check_overlap = TRUE) +
+  ggplot2::ggplot(DF, ggplot2::aes(y = pred, x = pos, col = type, shape = Model)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_hline(yintercept = c(0.25,  0.75), linetype = 3, size = 0.2) +
+    ggplot2::geom_hline(yintercept = 0.5, linetype = 3, size = 0.5, col = "black") +
+    ggplot2::geom_errorbar(ggplot2::aes(ymin = inf, ymax = sup), width= 0) +
+    ggplot2::geom_text(data = DF, ggplot2::aes(x = 2, y = 1.10), 
+              label = paste(DF$plotname[1]), 
+              size = 0.35*7, 
+              col = "black", 
+              inherit.aes = FALSE, 
+              check_overlap = TRUE) +
     # geom_bar(data = NULL, aes(x =4, y=10), inherit.aes = T) +
-    geom_hline(yintercept = 1.05) +
-    scale_color_manual(" ",
+    ggplot2::geom_hline(yintercept = 1.05) +
+    ggplot2::scale_color_manual(" ",
                        values = PPA_intra) +
-    scale_shape_manual(" ",
+    ggplot2::scale_shape_manual(" ",
                        values = c(15,17)) +
-    scale_x_continuous(limits = c(0.80,3.20),
+    ggplot2::scale_x_continuous(limits = c(0.80,3.20),
                        breaks = c(1, 2, 3),
                        labels =  c("Interclan",  "Intra-Mixed", "Intra-Native")) +
-    scale_y_continuous("Winning probability (%)",
+    ggplot2::scale_y_continuous("Winning probability (%)",
                        limits = c(0,1.10),
                        breaks = c(0, 0.25, 0.5, 0.75, 1),
                        labels = c("0", "25", "50", "75", "100")) +
     ggthemes::theme_base()  +
-    guides(col = FALSE, shape = FALSE)
+    ggplot2::guides(col = FALSE, shape = FALSE)
 }
 
 ################################################################################
-#' Plot2
+#' Plot1
 #'
-#' Creates the second plot of the paper
+#' Creates the first plot of the paper
 #'
-#'@name plot2
+#'@name plot1
 #'@param Mod_so_diff, social model different sex
 #'@param Mod_so_same, social model same sex
 #'@param Mod_mass_diff, mass model different sex
 #'@param Mod_mass_same, mass model same sex
 #'@param Mod_sex, sex model diff sex
+#'@param PDF, logical to return a PDF of the figure
 #'@import dplyr
 #'@import ggplot2
 #'@import ggthemes
@@ -212,18 +247,18 @@ plot_sex <- function(DF) {
 #'data(mod_mass_null_diff_PQL)
 #'data(mod_mass_null_same_PQL)
 #'data(mod_sex_null_diff_PQL)
-#'plot2(
+#'plot1(
 #'mod_social_null_diff_PQL,
 #'mod_social_null_same_PQL,
 #'mod_mass_null_diff_PQL,
 #'mod_mass_null_same_PQL,
-#'mod_sex_null_diff_PQL
-#')
-plot2 <- function(Mod_so_diff,
+#'mod_sex_null_diff_PQL, 
+#'PDF = TRUE)
+plot1 <- function(Mod_so_diff,
                   Mod_so_same,
                   Mod_mass_diff,
                   Mod_mass_same,
-                  Mod_sex) {
+                  Mod_sex, PDF) {
 
   type <- pred <- inf <- sup <- pos <- Model <- pos2 <- NULL
 
@@ -300,34 +335,58 @@ soss$pos2[soss$type == "mig"] <- 4
 
 ############# MAKE THE PLOT
 social_plot <- plot_intra_inter(soss) +
-  theme(text = element_text(size = 7),
-        plot.margin = margin(t = 3, r = 0, b = 3, l = 3, "pt"),
-        axis.text.x = element_text(angle = 45, hjust = 1, color = PPA_legend),
-        plot.background = element_blank(),
-        axis.ticks.length = unit(1, "mm"),
-        axis.title.x = element_blank(),
-        axis.title.y = element_text(margin = margin(r = -1)))
+  ggplot2::theme(
+        plot.margin = ggplot2::margin(t = 3, r = 0, b = 3, l = 3, "pt"),
+        axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, color = PPA_legend, 
+                                            size = 7, 
+                                            family = "", 
+                                            face = "plain"),
+        plot.background = ggplot2::element_blank(),
+        axis.ticks.length = ggplot2::unit(1, "mm"),
+        axis.title.x = ggplot2::element_blank(),
+        axis.title.y = ggplot2::element_text(margin = ggplot2::margin(r = -1)), 
+        text = ggplot2::element_text(size = 7, 
+                                     family = "",
+                                     face = "plain"))
 
 body_mass_plot <- plot_intra_inter(soss2) +
-  theme(text = element_text(size = 7),
-        plot.margin = margin(t = 3, r = 1, b = 3, l = 1, "pt"),
-        axis.text.x = element_text(angle = 45, hjust = 1, color = PPA_legend),
-        plot.background = element_blank(),
-        axis.title = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.length = unit(1, "mm"),
-        axis.ticks.y = element_blank())
+  ggplot2::theme(
+        plot.margin = ggplot2::margin(t = 3, r = 1, b = 3, l = 1, "pt"),
+        axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, color = PPA_legend, 
+                                            size = 7, 
+                                            family = "", 
+                                            face = "plain"),
+        plot.background = ggplot2::element_blank(),
+        axis.title = ggplot2::element_blank(),
+        axis.text.y = ggplot2::element_blank(),
+        axis.ticks.length = ggplot2::unit(1, "mm"),
+        axis.ticks.y = ggplot2::element_blank(), 
+        text = ggplot2::element_text(size = 7, 
+                                     family = "",
+                                     face = "plain"))
 
 sex_plot <- plot_sex(soss3) +
-  theme(text = element_text(size = 7),
-        plot.margin = margin(t = 3, r = 3, b = 3, l = 0, "pt"),
-        axis.text.x = element_text(angle = 45, hjust = 1, color = PPA_legend),
-        plot.background = element_blank(),
-        axis.title = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.length = unit(1, "mm"),
-        axis.ticks.y = element_blank())
+  ggplot2::theme(
+        plot.margin = ggplot2::margin(t = 3, r = 3, b = 3, l = 0, "pt"),
+        axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, color = PPA_legend, 
+                                            size = 7, 
+                                            family = "", 
+                                            face = "plain"),
+        plot.background = ggplot2::element_blank(),
+        axis.title = ggplot2::element_blank(),
+        axis.text.y = ggplot2::element_blank(),
+        axis.ticks.length = ggplot2::unit(1, "mm"),
+        axis.ticks.y = ggplot2::element_blank(), 
+        text = ggplot2::element_text(size = 7, 
+                                     family = "",
+                                     face = "plain"))
 
 ttt <- egg::ggarrange(social_plot, body_mass_plot, sex_plot, ncol = 3, widths = c(4,4,3))
+
+  if (PDF == TRUE) {
+  ggplot2::ggsave(ttt, filename = "plot1.pdf",  width = 88, height = 78, units= "mm")
+} else {
+ttt <- egg::ggarrange(social_plot, body_mass_plot, sex_plot, ncol = 3, widths = c(4,4,3))
 return(ttt)
+}
 }
