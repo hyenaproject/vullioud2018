8a9423111c6b59efd937e3a87a65e5c9f9e83a55
vullioud <colino.vullioud@gmail.com>
2018-10-05 17:10:12 +0200

    space

--- R/plots.R
+++ R/plots.R
@@ -362,8 +362,9 @@ return(ttt)
 #'@param mod social model different sex
 get_and_clean_prediction <- function(mod){
   type <- pred <- inf <- sup <- NULL
+  
   get_predictions(mod) %>%
-dplyr::group_by(type) %>%
+  dplyr::group_by(type) %>%
   dplyr::summarise(pred = pred[10],
                    inf = inf[10],
                    sup = sup[10])
