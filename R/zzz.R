90ea1bc1969acab64eaa271fe2b8bdedbedffaab
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2018-10-05 16:58:12 +0200

    new .onAttach function

---
+++ R/zzz.R
@@ -0,0 +1,18 @@
+.onAttach <- function(libname, pkgname) {
+  ## This function should not be called by the user.
+  ## It displays a message when the package is being loaded.
+  packageStartupMessage(## display message
+    "\n Welcome to vullioud2018,",
+    "\n ",
+    "\n This package has not been conceived for general use.",
+    "\n It has only been created to document the analysis of the paper:",
+    "\n ",
+    "\n Social support drives female dominance in the spotted hyena",
+    "\n ",
+    "\n by Vullioud, Davidian, Wachter, Rousset, Courtiol and Höner,",
+    "\n published in Nature Ecology Evolution (2018).",
+    "\n ",
+    "\n Type ?vullioud2018 for information about how to use this package!",
+    "\n"
+  )
+}
